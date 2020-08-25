806c2ea1a5f3e00b0d36429b1dcf4d3cb1f887b8
Simon John <817564+sej7278@users.noreply.github.com>
2020-08-25 11:16:27 +0100

    Added hyper-v enlightenments to win10
    also using newer 2004 iso and virtio-win

--- win10uefi.sh
+++ win10uefi.sh
@@ -4,17 +4,18 @@ virt-install \
 --virt-type kvm \
 --name=win10uefi \
 --os-variant=win10 \
---vcpus 2 \
+--vcpus 16 \
 --cpu host-passthrough \
---memory 4096 \
---features kvm_hidden=on \
---disk path=~/win10uefi.img,size=50,format=raw,sparse=true,bus=scsi,cache=writethrough,discard=unmap,io=threads  \
+--memory 16384 \
+--features kvm_hidden=on,hyperv_relaxed=on,hyperv_vapic=on,hyperv_spinlocks=on,hyperv_spinlocks_retries=8191 \
+--clock hypervclock_present=yes \
+--disk path=~/win10uefi.qcow2,size=50,format=qcow2,sparse=true,bus=scsi,cache=writethrough,discard=unmap,io=threads  \
 --controller type=scsi,model=virtio-scsi \
 --channel unix,target_type=virtio,name=org.qemu.guest_agent.0 \
 --network bridge=br0,model=virtio \
 --metadata title='Windows 10 (VFIO)' \
---disk ~/virtio-win-0.1.172.iso,device=cdrom \
---cdrom ~/Win10_1903_V2_English_x64.iso \
+--disk ~/virtio-win-0.1.185.iso,device=cdrom \
+--cdrom ~/Win10_2004_English_x64.iso \
 --graphics none \
 --hostdev 02:00.0,address.type=pci,address.multifunction=on \
 --hostdev 02:00.1,address.type=pci \
@@ -25,16 +26,8 @@ virt-install \
 
 <<COMMENTS
 
-nvidia error 43 workaround - this is a bit naff, but at least it uses virt-install:
+virsh edit win10uefi
 
-    --qemu-commandline='-cpu host,hv_vendor_id=ab1234567890' \
-
-it seems better to do it via "virsh edit":
-
-    <hyperv>
-      <vendor_id state="on" value="ab1234567890"/>
-    </hyperv>
-
-I'm using raw disk image instead of qcow2 for the moment as there appears to be a bug in qemu 4.1 that is corrupting qcow2 images on win10 at least.
+    <vendor_id state='on' value='ab1234567890'/>
 
 COMMENTS
