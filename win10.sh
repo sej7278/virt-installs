acf588732872f777b76918782539c8bcc77cc91e
Simon John <817564+sej7278@users.noreply.github.com>
2020-08-30 00:34:04 +0100

    Added hyper-v enlightenments to qxl win10
    also updated virtio-win and win10 iso's.

--- win10.sh
+++ win10.sh
@@ -7,8 +7,9 @@ virt-install \
 --vcpus 2 \
 --cpu host-passthrough \
 --memory 4096 \
---features kvm_hidden=on \
---disk path=~/win10.img,size=50,format=raw,sparse=true,bus=scsi,discard=unmap,io=threads  \
+--features kvm_hidden=on,hyperv_relaxed=on,hyperv_vapic=on,hyperv_spinlocks=on,hyperv_spinlocks_retries=8191 \
+--clock hypervclock_present=yes \
+--disk path=~/win10.qcow2,size=50,format=qcow2,sparse=true,bus=scsi,cache=writethrough,discard=unmap,io=threads  \
 --controller type=scsi,model=virtio-scsi \
 --graphics spice \
 --video model=qxl,vgamem=32768,ram=131072,vram=131072,heads=1 \
@@ -17,6 +18,6 @@ virt-install \
 --network bridge=br0,model=virtio \
 --input type=tablet,bus=virtio \
 --metadata title='Windows 10' \
---disk ~/virtio-win-0.1.173.iso,device=cdrom \
---cdrom ~/Win10_1909_English_x64.iso \
+--disk ~/virtio-win-0.1.185.iso,device=cdrom \
+--cdrom ~/Win10_2004_English_x64.iso \
 --boot menu=on
