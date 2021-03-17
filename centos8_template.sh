f74635e9085866c7444c38e42518a5a07bd2e4de
Simon John <817564+sej7278@users.noreply.github.com>
2021-03-17 11:48:06 +0000

    updated centos8 for 8.3
    removed some confusing comments, scaled up so you can run k8's inside without running out of resources

--- centos8_template.sh
+++ centos8_template.sh
@@ -2,32 +2,24 @@
 
 virt-install \
 --virt-type kvm \
---name=centos8_template \
+--name=centos8 \
 --os-variant=centos8 \
 --cpu host-passthrough \
---vcpus 2 \
---memory 2048 \
---disk path=~/centos8_template.qcow2,size=40,format=qcow2,sparse=true,bus=scsi,discard=unmap \
+--vcpus 4 \
+--memory 4096 \
+--disk path=~/centos8.qcow2,size=40,format=qcow2,sparse=true,bus=scsi,discard=unmap \
 --controller type=scsi,model=virtio-scsi \
 --network bridge=br0,model=virtio \
 --channel unix,target_type=virtio,name=org.qemu.guest_agent.0 \
 --graphics none \
 --video none \
---metadata title="CentOS 8.2 (template)" \
---location ~/CentOS-8.2.2004-x86_64-dvd1.iso \
+--metadata title="CentOS 8.3" \
+--location ~/CentOS-8.3.2011-x86_64-dvd1.iso \
 --extra-args='console=tty0 console=ttyS0,115200n8 serial'
 
 <<COMMENTS
 
 systemctl enable fstrim.timer
 systemctl start fstrim.timer
-
-ctrl-shift-] to exit virsh console
-
-virsh shutdown centos8_template
-virt-clone --original centos8_template --name centos8_test --file ~/centos8_test.qcow2
-virt-sysprep -d centos8_test
-virsh start centos8_test
-virsh console centos8_test
 
 COMMENTS
