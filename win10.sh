b46a61f3f9cf940b00da7df280f23c7ed753a3a2
Simon John <817564+sej7278@users.noreply.github.com>
2021-06-26 11:23:53 +0100

    TPM and SecureBoot for win10
    Switched the win10 spice script to enable TPM2.0 (using swtpm) and UEFI with MS signed SecureBoot
    
    Also switched back to qcow2 and made the disk bigger (win11 will require 64gb+)

--- win10.sh
+++ win10.sh
@@ -7,9 +7,9 @@ virt-install \
 --vcpus 2 \
 --cpu host-passthrough \
 --memory 4096 \
---features kvm_hidden=on,hyperv_relaxed=on,hyperv_vapic=on,hyperv_spinlocks=on,hyperv_spinlocks_retries=8191 \
+--features smm.state=on,kvm_hidden=on,hyperv_relaxed=on,hyperv_vapic=on,hyperv_spinlocks=on,hyperv_spinlocks_retries=8191 \
 --clock hypervclock_present=yes \
---disk path=~/win10.qcow2,size=50,format=qcow2,sparse=true,bus=scsi,cache=writethrough,discard=unmap,io=threads  \
+--disk path=~/win10.qcow2,size=100,format=qcow2,sparse=true,bus=scsi,cache=writethrough,discard=unmap,io=threads  \
 --controller type=scsi,model=virtio-scsi \
 --graphics spice \
 --video model=qxl,vgamem=32768,ram=131072,vram=131072,heads=1 \
@@ -17,7 +17,8 @@ virt-install \
 --channel unix,target_type=virtio,name=org.qemu.guest_agent.0 \
 --network bridge=br0,model=virtio \
 --input type=tablet,bus=virtio \
---metadata title='Windows 10' \
---disk ~/virtio-win-0.1.185.iso,device=cdrom \
---cdrom ~/Win10_2004_English_x64.iso \
---boot menu=on
+--metadata title='Win10 (TPM2 and SB)' \
+--disk ~/virtio-win-0.1.190.iso,device=cdrom \
+--cdrom ~/Win10_21H1_English_x64.iso \
+--tpm type=emulator,version=2.0,model=tpm-tis \
+--boot loader=/usr/share/OVMF/OVMF_CODE_4M.secboot.fd,loader.readonly=yes,loader.type=pflash,loader.secure=yes,nvram.template=/usr/share/OVMF/OVMF_VARS_4M.ms.fd,menu=on
