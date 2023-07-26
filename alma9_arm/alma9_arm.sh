80ec1c55ce91f5174e2b6aa04602228f4a612f2f
Simon John <817564+sej7278@users.noreply.github.com>
2023-07-26 17:32:02 +0100

    Updated alma9_arm.sh to 9.2
    also added cpu type flag

--- alma9_arm/alma9_arm.sh
+++ alma9_arm/alma9_arm.sh
@@ -4,6 +4,7 @@ virt-install \
 --name=alma9_arm \
 --os-variant=almalinux9 \
 --arch aarch64 \
+--cpu cortex-a72 \
 --vcpus 4 \
 --memory 4096 \
 --disk path=/kvm/alma9_arm.qcow2,size=40,format=qcow2,sparse=true,bus=scsi,discard=unmap \
@@ -11,8 +12,8 @@ virt-install \
 --network bridge=br0,model=virtio \
 --channel unix,target_type=virtio,name=org.qemu.guest_agent.0 \
 --graphics none \
---metadata title="AlmaLinux 9.1 (aarch64)" \
---location /kvm/AlmaLinux-9.1-aarch64-minimal.iso \
+--metadata title="AlmaLinux 9.2 (aarch64)" \
+--location /kvm/AlmaLinux-9.2-aarch64-minimal.iso \
 --extra-args='inst.ks=file:alma9_arm.cfg' \
 --initrd-inject=alma9_arm.cfg \
 --boot loader=/usr/share/AAVMF/AAVMF_CODE.fd,loader_ro=yes,loader_type=pflash,nvram.template=/usr/share/AAVMF/AAVMF_VARS.fd
